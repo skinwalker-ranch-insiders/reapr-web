f2319bb2958ccdef8f32cf31855ccb5433301116
RK Davies <rkdavies@RKs-MacBook-Pro.local>
2022-12-07 11:57:02 -0800

    Adding filters and export

--- app.py
+++ app.py
@@ -17,6 +17,51 @@ def get_data():
     connection.close()
     return data
 
+def get_events():
+    ###### SQL CONNECTION ######
+    connection = mysql.connector.connect(
+                                host=db_server,
+                                database=db_name,
+                                user=db_user,
+                                password=db_passwd)
+
+    cursor = connection.cursor()
+    query = ("SELECT * FROM yt_events WHERE YT_Tag = 'EVENT' ORDER BY id DESC")
+    cursor.execute(query)
+    data = cursor.fetchall()
+    connection.close()
+    return data
+
+def get_thoughts():
+    ###### SQL CONNECTION ######
+    connection = mysql.connector.connect(
+                                host=db_server,
+                                database=db_name,
+                                user=db_user,
+                                password=db_passwd)
+
+    cursor = connection.cursor()
+    query = ("SELECT * FROM yt_events WHERE YT_Tag = 'THOUGHT' ORDER BY id DESC")
+    cursor.execute(query)
+    data = cursor.fetchall()
+    connection.close()
+    return data
+
+def get_requests():
+    ###### SQL CONNECTION ######
+    connection = mysql.connector.connect(
+                                host=db_server,
+                                database=db_name,
+                                user=db_user,
+                                password=db_passwd)
+
+    cursor = connection.cursor()
+    query = ("SELECT * FROM yt_events WHERE YT_Tag = 'REQUEST' ORDER BY id DESC")
+    cursor.execute(query)
+    data = cursor.fetchall()
+    connection.close()
+    return data
+
 ###### FLASK SERVER ######
 title = "REAPR Web - Tagged LiveStream Events"
 headings = ("ID", "Tag", "Date/Time", "User", "Message")
@@ -29,3 +74,39 @@ def index():
 
     if (__name__ == '__main__'):
         app.run(debug=False)
+
+@app.route('/events/')
+def events():
+    data = get_events()
+    return render_template("table.html", headings=headings, data=data, title=title)
+
+    if (__name__ == '__main__'):
+        app.run(debug=False)
+
+@app.route('/thoughts/')
+def thoughts():
+    data = get_thoughts()
+    return render_template("table.html", headings=headings, data=data, title=title)
+
+    if (__name__ == '__main__'):
+        app.run(debug=False)
+
+@app.route('/requests/')
+def requests():
+    data = get_requests()
+    return render_template("table.html", headings=headings, data=data, title=title)
+
+    if (__name__ == '__main__'):
+        app.run(debug=False)
+
+@app.route('/export/', methods=['GET'])
+def export():
+    si = StringIO()
+    cw = csv.writer(si)
+    rows = get_data()
+    cw.writerow([heading[0] for heading in headings])
+    cw.writerows(rows)
+    response = make_response(si.getvalue())
+    response.headers['Content-Disposition'] = 'attachment; filename=REAPR-report.csv'
+    response.headers["Content-type"] = "text/csv"
+    return response
