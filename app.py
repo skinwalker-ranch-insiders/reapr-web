fed8df5023a904ccb8af631e1666d389c57ab75f
John Neiberger <jneiberger@gmail.com>
2022-12-15 20:10:51 -0700

    PEP8 formatting and simplified multiple functions into a single get_activity function

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 # and the data collected with REAPR from the Skinwalker Ranch
 # Insiders program.
 #
-# The intent is be able to log observations quickly from YT 
-# Chat and save them in a database for later review. To 
+# The intent is be able to log observations quickly from YT
+# Chat and save them in a database for later review. To
 # support reading, sorting, filtering and exporting this data
 # REAPR-web was developed.
 #
@@ -18,145 +18,94 @@ from settings import db_server, db_user, db_passwd, db_name
 from flask import render_template, make_response, Flask
 from io import StringIO
 
-def get_data():
-    ###### SQL CONNECTION ######
-    connection = mysql.connector.connect(
-                                host=db_server,
-                                database=db_name,
-                                user=db_user,
-                                password=db_passwd)
 
-    cursor = connection.cursor()    
-    query = ("SELECT * FROM yt_events ORDER BY id DESC")
-    cursor.execute(query)
-    data = cursor.fetchall()
-    connection.close()
-    return data
-
-def get_events():
-    ###### SQL CONNECTION ######
+def load_all_from_db():
+    """Retrieve all data from database"""
     connection = mysql.connector.connect(
-                                host=db_server,
-                                database=db_name,
-                                user=db_user,
-                                password=db_passwd)
+        host=db_server, database=db_name, user=db_user, password=db_passwd
+    )
 
     cursor = connection.cursor()
-    query = ("SELECT * FROM yt_events WHERE YT_Tag = 'EVENT' ORDER BY id DESC")
+    query = "SELECT * FROM yt_events ORDER BY id DESC"
     cursor.execute(query)
     data = cursor.fetchall()
     connection.close()
     return data
 
-def get_thoughts():
-    ###### SQL CONNECTION ######
-    connection = mysql.connector.connect(
-                                host=db_server,
-                                database=db_name,
-                                user=db_user,
-                                password=db_passwd)
-    cursor = connection.cursor()
-    query = ("SELECT * FROM yt_events WHERE YT_Tag = 'THOUGHT' ORDER BY id DESC")
-    cursor.execute(query)
-    data = cursor.fetchall()
-    connection.close()
-    return data
 
-def get_requests():
-    ###### SQL CONNECTION ######
-    connection = mysql.connector.connect(
-                                host=db_server,
-                                database=db_name,
-                                user=db_user,
-                                password=db_passwd)
-    cursor = connection.cursor()
-    query = ("SELECT * FROM yt_events WHERE YT_Tag = 'REQUEST' ORDER BY id DESC")
-    cursor.execute(query)
-    data = cursor.fetchall()
-    connection.close()
-    return data
+def get_activity(activity_type):
+    """Load specified activity type from database"""
+    with mysql.connector.connect(
+        host=db_server, database=db_name, user=db_user, password=db_passwd
+    ) as connection:
+        cursor = connection.cursor()
+        query = (
+            f"SELECT * FROM yt_events WHERE YT_Tag = '{activity_type}' ORDER BY id DESC"
+        )
+        cursor.execute(query)
+        data = cursor.fetchall()
+        connection.close()
+        return data
 
-def get_feedback():
-    ###### SQL CONNECTION ######
-    connection = mysql.connector.connect(
-                                host=db_server,
-                                database=db_name,
-                                user=db_user,
-                                password=db_passwd)
-    cursor = connection.cursor()
-    query = ("SELECT * FROM yt_events WHERE YT_Tag = 'FEEDBACK' ORDER BY id DESC")
-    cursor.execute(query)
-    data = cursor.fetchall()
-    connection.close()
-    return data
 
 ###### FLASK SERVER ######
 title = "REAPR Web - Tagged LiveStream Events"
 headings = ("ID", "Tag", "Date/Time", "User", "Message", "In SS")
 
 app = Flask(__name__, template_folder="template")
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-@app.route('/')    
-def index(): 
-    data = get_data()
-    return render_template("table.html", 
-                            headings=headings, 
-                            data=data, 
-                            title=title)
-    if (__name__ == '__main__'):
+
+@app.route("/")
+def index():
+    data = load_all_from_db()
+    return render_template("table.html", headings=headings, data=data, title=title)
+    if __name__ == "__main__":
         app.run(debug=False)
 
-@app.route('/events/')
+
+@app.route("/events/")
 def events():
-    data = get_events()
-    return render_template("table.html", 
-                            headings=headings, 
-                            data=data, 
-                            title=title)
-    if (__name__ == '__main__'):
+    data = get_activity("EVENT")
+    return render_template("table.html", headings=headings, data=data, title=title)
+    if __name__ == "__main__":
         app.run(debug=False)
 
-@app.route('/thoughts/')
+
+@app.route("/thoughts/")
 def thoughts():
-    data = get_thoughts()
-    return render_template("table.html", 
-                            headings=headings, 
-                            data=data, 
-                            title=title)
-    if (__name__ == '__main__'):
+    data = get_activity("THOUGHT")
+    return render_template("table.html", headings=headings, data=data, title=title)
+    if __name__ == "__main__":
         app.run(debug=False)
 
-@app.route('/requests/')
+
+@app.route("/requests/")
 def requests():
-    data = get_requests()
-    return render_template("table.html", 
-                            headings=headings, 
-                            data=data, 
-                            title=title)
-    if (__name__ == '__main__'):
+    data = get_activity("REQUEST")
+    return render_template("table.html", headings=headings, data=data, title=title)
+    if __name__ == "__main__":
         app.run(debug=False)
 
-@app.route('/feedback/')
+
+@app.route("/feedback/")
 def feedback():
-    data = get_feedback()
-    return render_template("table.html", 
-                            headings=headings, 
-                            data=data, 
-                            title=title)
-    if (__name__ == '__main__'):
+    data = get_activity("FEEDBACK")
+    return render_template("table.html", headings=headings, data=data, title=title)
+    if __name__ == "__main__":
         app.run(debug=False)
 
-@app.route('/export/', methods=['GET'])
+
+@app.route("/export/", methods=["GET"])
 def export():
     si = StringIO()
     cw = csv.writer(si)
-    rows = get_data()
+    rows = load_all_from_db()
     cw.writerow([heading[0] for heading in headings])
     cw.writerows(rows)
     response = make_response(si.getvalue())
-    response.headers['Content-Disposition'] = 'attachment; filename=REAPR-report.csv'
+    response.headers["Content-Disposition"] = "attachment; filename=REAPR-report.csv"
     response.headers["Content-type"] = "text/csv"
     return response
-    if (__name__ == '__main__'):
+    if __name__ == "__main__":
         app.run(debug=False)
